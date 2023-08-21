2f579bf4d0f4dac4c78801d314a225f8313e78ce
brenobernardes <breno.pbernardes@gmail.com>
2023-08-21 16:52:17 -0300

    created header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,33 @@
+import styles from './styles.module.scss';
+import Image from 'next/image';
+import logo from '../../../public/images/logo.svg';
+
+import Link from 'next/link';
+
+export default function Header() {
+    return (
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <a>
+                    <Image src={logo} alt='Sujeito Programador Logo'/>
+                </a>
+
+                <nav>
+                    <Link legacyBehavior href='/'>
+                        <a>Home</a>
+                    </Link>
+
+                    <Link legacyBehavior href='/posts'>
+                        <a>Conteúdos</a>
+                    </Link>
+
+                    <Link legacyBehavior href='/sobre'>
+                        <a>Quem somos</a>
+                    </Link>
+                </nav>
+
+                <a className={styles.readyButton} type='button' href='https://sujeitoprogramador.com'>Começar</a>
+            </div>
+        </header>
+    )
+}
